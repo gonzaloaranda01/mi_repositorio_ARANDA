fceb2d5ee5c34785f87efdf40186bf65641b0e28
gonzaloaranda01 <gonzaloaranda.ing@gmail.com>
2015-09-25 13:02:21 -0300

    examen

---
+++ mi_proyecto_evaluacion/src/mi_proyecto_evaluacion.c
@@ -0,0 +1,297 @@
+/* Copyright 2015, ARANDA GONZALO
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Blinking Bare Metal example source file
+ **
+ ** This is a mini example of the CIAA Firmware.
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example source file
+ ** @{ */
+/*
+/*
+ * -----------------------------------------------------------
+ * yyyymmdd v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "mi_proyecto_evaluacion.h"       /* <= own header */
+
+/******************************************************************************/
+/*ARANDA GONZALO*/
+/*MAIL gonzaloaranda.ing@gmail.com*/
+
+/******************************************************************************/
+/*
+ FUNCIONEs
+Tec 1: Aumento la ganancia"
+Tec 2: Disminuyo la ganancia"
+Tec 3: MUTE"
+Tec 4: ..." */
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+//external void ISR_timer(void);
+
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+
+# define LED_rojo 5
+# define LED_verde 6
+# define LED_naranja 4
+# define LED_RGB_v 2
+# define LED_RGB_a 3
+# define LED_RGB_r 1
+
+# define TECLA_1 1
+
+# define PULSADOR_4 4
+# define PULSADOR_3 3
+# define PULSADOR_2 2
+# define PULSADOR_1 1
+
+# define TIME 100    // 100 mseg --> 10hz
+
+// ADC 1024---------------3.3v
+
+ int Count = 0;
+ float signal = 0;
+ float ganancia = 2;
+ float divisor = 0.5;
+ int aux_led = 0;
+ int aux_anterior = 0;
+ /***********************************************************/
+ //Declaracion de funciones
+ float aumento_ganancia(float Signal,float Gain);
+ float disminuir_ganancia(float Signal,float Divisor);
+ float MUTE(float Signal);
+ int Receiver_Signal(void);
+ void parpadear_led(void);
+ /***********************************************************/
+
+ /***************************************************************/
+ /*****funcion interrupcion**************************************/
+ /***************************************************************/
+ void ISR_timer (void)
+{
+	timer_off();
+	Count++;
+	signal = (float)Receiver_Signal();
+
+    if (aux_led == 0)
+    {
+         aux_led  = 1;
+         //encender_led(LED_RGB_v);
+    }
+    else if (aux_led ==1)
+    {
+    	aux_led = 0;
+    	//apagar_led(LED_RGB_v);
+    }
+    aux_led;
+}
+ /***************************************************************/
+  /********************MAIN**************************************/
+  /***************************************************************/
+int main(void)
+{
+         /* perform the needed initialization here */
+
+	    /*variables de uso*/
+	    int Tecla=0;
+     	int Rojo,Verde,Naranja,Blanco;
+        int Tecla_1 = 0,Tecla_2 = 0,Tecla_3 = 0,Tecla_4 = 0;
+        int aux = 0;
+        int parpadeoCrtl =0;
+        int parpadeoCrtl_2 =0;
+        //int signal=0;
+
+    	   Rojo = Verde = Naranja = Blanco =0;
+
+	 	/*****************inicializo drivers************/
+		inicializar_led();
+        inicializarTeclas();
+        inicializar_ADC();
+        inicializar_DAC();
+        configurar_UART();
+
+        /*************** timer*************************/
+        inicializarTimer(TIME);
+        habilitoInterrupcion();
+        /****************loop**************************/
+		while(1)
+		{
+			Tecla = tecla_on();  // devuelve el valor de la Tecla
+			//-----------------------------------------------------------------
+			if(parpadeoCrtl)
+				parpadear_led();
+			else
+			{
+				apagar_led(LED_RGB_a);
+				apagar_led(LED_RGB_v);
+				apagar_led(LED_RGB_r);
+			}
+			//------------------------------------------------------------------
+			//-----------------------------------------------------------------
+
+			if (Tecla == PULSADOR_2)
+			{
+				if( Naranja == 1)
+				{
+					Naranja = 0;
+					apagar_led(LED_naranja);
+				}
+			 	else
+				{
+				Naranja = 1;
+				encender_led(LED_naranja);
+			    /* funcion que disminuir la ganancia de la señal*/
+				signal = disminuir_ganancia(signal,divisor);
+				escribir_DAC((int)signal);    // escribo DAC
+				}
+
+			}
+			//----------------------------------------------------------------------------------------------------------
+			if (Tecla == PULSADOR_3)
+			{
+				if( Rojo == 1)
+				{
+					Rojo = 0;
+					apagar_led(LED_rojo);
+				}
+				else
+				{
+					Rojo = 1;
+					encender_led(LED_rojo);
+					MUTE(signal);                 /* funcion de MUTE--> valores Sginal = 0*/
+					escribir_DAC((int)signal);    // escribo DAC
+				}
+			}
+			//----------------------------------------------------------------------------------------------------------
+			if (Tecla == PULSADOR_1)
+			{
+				    Blanco = 1;
+					parpadeoCrtl=!parpadeoCrtl;
+					signal = aumento_ganancia(signal,ganancia);           /* funcion que aumenta la ganancia de la señal*/
+					escribir_DAC((int)signal);
+			}
+			//-----------------------------------------------------------------------------------------------------------------
+			if (Tecla == PULSADOR_4)
+			{
+				if( Verde == 1)
+				{
+					Verde = 0;
+					apagar_led(LED_verde);
+				}
+				else
+				{
+					Verde = 1;
+					encender_led(LED_verde);
+
+				}
+			}
+
+        }
+}
+/****** funciones ****/
+void parpadear_led(void)
+{
+
+
+	if ( aux_led==1)
+	{
+		encender_led(LED_RGB_v);
+		encender_led(LED_RGB_r);
+    	encender_led(LED_RGB_a);
+	}
+
+	else if( aux_led==0)
+	{
+		apagar_led(LED_RGB_a);
+		apagar_led(LED_RGB_v);
+		apagar_led(LED_RGB_r);
+	}
+}
+
+int Receiver_Signal(void)
+{
+	int datoadc;
+	datoadc = leer_ADC();
+    return datoadc;
+}
+
+float MUTE(float Signal)
+{
+	Signal = 0;
+    return Signal;
+}
+
+float aumento_ganancia(float Signal,float Gain)
+{
+
+   Signal = Signal*Gain;
+   return Signal;
+
+}
+
+float disminuir_ganancia(float Signal,float Divisor)
+{
+
+	   Signal = Signal*Divisor;
+	   return Signal;
+}
+/*==================[end of file]============================================*/
+
